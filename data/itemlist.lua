538a05b47e86e5a61451405e42f118ba2b8cfbb6
BleuLlama <yorgle@gmail.com>
2013-08-24 00:09:57 -0400

    itemlist is new.

---
+++ data/itemlist.lua
@@ -0,0 +1,9 @@
+-- list of all of the items.
+
+AddItem( "carrot", 10, "room0" )
+AddItem( "slime", -5, "itemtest" )
+AddItem( "cheese", 5, "itemtest" )
+AddItem( "key", 0, "PLAYER" )
+AddItem( "taco", 1, "chessroom" )
+
+
